2654fab76e63262edbddd28c0746f4f3508c420f
Jerry Chen <jerryc830@gmail.com>
2023-06-20 16:59:44 -0400

    prisma

--- server/db/conn.js
+++ server/db/conn.js
@@ -5,6 +5,6 @@ const config = {
 if(process.env.QUIET){
   config.logging = false;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/zapquest', config);
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_shopping_db', config);
 
 module.exports = conn;
