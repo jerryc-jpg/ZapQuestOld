aac67794a766fe63fec8a6b7755793d728a0d3f9
Yimin Yang <yvonne.yangh@gmail.com>
2023-06-21 16:04:27 -0500

    Update Map
    can add marker base on an input address

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,58 +1,98 @@
 import React, { useState } from "react";
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
+import axios from "axios";
 
 
 const Map = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyAyylWo4yRMjT_HSowB1jWsz5qwnPDSUWw',
-  });
-  const center = useMemo(() => ({ lat: 40.7128, lng: -74.0060 }), []);
-  const [myLatitude, setMyLatitude]=useState(null);
-  const [myLongitute, setMyLongitute]=useState(null);
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyAyylWo4yRMjT_HSowB1jWsz5qwnPDSUWw',
+    });
+    const center = useMemo(() => ({ lat: 40.7128, lng: -74.0060 }), []);
+    const [myLocation, setMyLocation] = useState(null);
+    const [searchLocation, setSearchLocation] = useState(null);
+    const [addressList,setAddressList] = useState([center]);
 
-  const handleLocalLocation = () => {
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(
-            (position) =>{
-                const {latitude, longitude} = position.coords
-                console.log(`Latitude: ${latitude}`,typeof latitude);
-                console.log(`Longitude: ${longitude}`);
-                setMyLatitude(latitude);
-                setMyLongitute(longitude);
+    const handleLocalLocation = () => {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const { latitude, longitude } = position.coords;
+              console.log(`Latitude: ${latitude}`, typeof latitude);
+              console.log(`Longitude: ${longitude}`);
+              const newLocation = { lat: latitude, lng: longitude };
+              setMyLocation(newLocation);
+              setAddressList((prevAddressList) => [...prevAddressList, newLocation]);
             },
-            (error) =>{
-                console.error(error.message);
+            (error) => {
+              console.error(error.message);
             }
-        )
-    }
-  }
+          );
+        }
+      };
+      
+      const handleSearchGeo = async () => {
+        let location = '22 Main st Boston MA';
+        try {
+          const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+              address: location,
+              key: 'AIzaSyAyylWo4yRMjT_HSowB1jWsz5qwnPDSUWw', // Replace with your own API key
+            },
+          });
+          console.log(response);
+          const mySearchLocation = response.data.results[0].geometry.location;
+          console.log(mySearchLocation);
+          setSearchLocation(mySearchLocation);
+          setAddressList((prevAddressList) => [...prevAddressList, mySearchLocation]);
+        } catch (ex) {
+          console.log(ex);
+        }
+      };
 
-  return (
-    <div className="Map">
-      {!isLoaded ? (
-        <h1>Loading...</h1>
-      ) : (
-        <GoogleMap
-        mapContainerClassName="map-container"
-        center={center}
-        zoom={10}
-      >
-        <Marker 
-            position={{ lat: 40.7128, lng: -74.0060 }} 
-            icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
-        />
-        {myLatitude?
-        ( <Marker 
-            position={{ lat: myLatitude, lng: myLongitute }} />
-        )
-        :(null)}
-        
-      </GoogleMap>
-      )}
-      <button className="map-button" onClick={handleLocalLocation}>Button</button>
-    </div>
-  );
+    const onLoad = (map) => {
+        const bounds = new google.maps.LatLngBounds();
+        addressList?.forEach(({lat,lng}) =>{
+            bounds.extend({lat,lng})
+        });
+        map.fitBounds(bounds);
+    }
+    
+
+    console.log(addressList);
+    return (
+        <div className="Map">
+        {!isLoaded ? (
+            <h1>Loading...</h1>
+        ) : (
+            <GoogleMap
+            mapContainerClassName="map-container"
+            onLoad={onLoad}
+        >
+            <Marker 
+                position={center} 
+                icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+            />
+            {myLocation?
+            ( <Marker 
+                position={myLocation} />
+            )
+            :(null)}
+
+            {searchLocation?
+            ( <Marker 
+                position={searchLocation} 
+                icon={"http://maps.google.com/mapfiles/ms/icons/pink-dot.png"}/>
+            )
+            :(null)
+            }
+            
+        </GoogleMap>
+        )}
+        <button className="map-button" onClick={handleLocalLocation}>Button</button>
+        <button onClick={handleSearchGeo}>getGeoCode</button>
+        </div>
+    );
 };
 
 export default Map;
