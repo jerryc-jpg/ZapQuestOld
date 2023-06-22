52b00b0e27db76acf845f6f38c012697dc2694a7
Neurotrip <ironlightcreative@gmail.com>
2023-06-22 14:43:55 -0500

    Added some routes and placeholder files

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import Home from "./Home";
 import Login from "./Login";
 import Navbar from "./Navbar";
+import Nearby from "./Nearby"
+import Map from "./Map"
 import { useSelector, useDispatch } from "react-redux";
 import { loginWithToken, fetchCart } from "../store";
 import { Link, Routes, Route } from "react-router-dom";
@@ -25,6 +27,8 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/nearby" element={<Nearby />} />
+          <Route path="/map" element={<Map />} />
         </Routes>
       </div>
     </div>
